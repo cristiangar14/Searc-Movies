56fab13d1c3ff11a1a66f8f0a23f25834dfeb679
Cristian Garzon Sanchez <desarrollo.cristian.garzon@gmail.com>
2020-07-20 23:17:58 -0500

    primer comit

---
+++ src/pages/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ButtonBackToHome } from '../components/ButtonBackToHome';
+
+export const NotFound = () => (
+    <div>
+        <h1 className='title is-1 is-spaced'>404!</h1>
+        <h2 className='subtitle'>No existe la página</h2>
+        <ButtonBackToHome/>
+    </div>
+)
